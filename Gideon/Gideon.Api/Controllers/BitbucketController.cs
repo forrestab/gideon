9ebc87b3bdc40420598e1cb1707cf21b754b76dc
Forrest Brown <forrestabrown@gmail.com>
2018-02-20 09:20:33 -0500

    feat: add basic web api project

---
+++ Gideon/Gideon.Api/Controllers/BitbucketController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Gideon.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class BitbucketController : Controller
+    {
+        private readonly ILogger logger;
+
+        public BitbucketController(ILoggerFactory loggerFactory)
+        {
+            this.logger = loggerFactory.CreateLogger<BitbucketController>();
+        }
+    }
+}
