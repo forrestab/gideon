6bb34d1440202b13a75639683e2b8f4cf4d1de8c
Forrest Brown <forrestabrown@gmail.com>
2018-02-20 19:54:06 -0500

    fix: remove web hook parameters

--- Gideon/Gideon.WebHooks.Receivers.BitbucketServer/Metadata/BitbucketMetadata.cs
+++ Gideon/Gideon.WebHooks.Receivers.BitbucketServer/Metadata/BitbucketMetadata.cs
@@ -5,26 +5,12 @@ using System.Text;
 
 namespace Gideon.WebHooks.Receivers.BitbucketServer.Metadata
 {
-    public class BitbucketMetadata : WebHookMetadata, IWebHookBindingMetadata, IWebHookBodyTypeMetadataService, IWebHookEventMetadata
+    public class BitbucketMetadata : WebHookMetadata, IWebHookBodyTypeMetadataService, IWebHookEventMetadata
     {
         public WebHookBodyType BodyType => WebHookBodyType.Json;
         public string ConstantValue => null;
         public string HeaderName => BitbucketConstants.EventHeaderName;
         public string QueryParameterName => null;
-
-        public IReadOnlyList<WebHookParameter> Parameters { get; } = new WebHookParameter[]
-        {
-            new WebHookParameter(
-                BitbucketConstants.RequestIdHeaderName,
-                WebHookParameterType.Header,
-                BitbucketConstants.RequestIdParameterName,
-                isRequired: false),
-            new WebHookParameter(
-                BitbucketConstants.HubSignatureHeaderName,
-                WebHookParameterType.Header,
-                BitbucketConstants.HubSignatureParameterName,
-                isRequired: true)
-        };
 
         public BitbucketMetadata()
             : base(BitbucketConstants.ReceiverName)
