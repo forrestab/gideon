a67de187f8f84600b11ec4ddcef65a5ec3259c27
Forrest Brown <forrestabrown@gmail.com>
2018-03-05 21:16:04 -0500

    fix: add string representation of `NeedsWork` enum value

--- Gideon/Gideon.WebHooks.Receivers.BitbucketServer/Models/Enums/BitbucketStatus.cs
+++ Gideon/Gideon.WebHooks.Receivers.BitbucketServer/Models/Enums/BitbucketStatus.cs
@@ -1,8 +1,12 @@
-﻿namespace Gideon.WebHooks.Receivers.BitbucketServer.Models.Enums
+﻿using System.Runtime.Serialization;
+
+namespace Gideon.WebHooks.Receivers.BitbucketServer.Models.Enums
 {
     public enum BitbucketStatus
     {
         Approved,
+        [EnumMember(Value = "NEEDS_WORK")]
+        NeedsWork,
         Unapproved
     }
 }
