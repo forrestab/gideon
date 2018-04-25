c6a9716100e6e628f869299b3e6fefd2e1d8a223
Forrest Brown <forrestabrown@gmail.com>
2018-04-25 08:19:54 -0400

    refactor: pull hard-coded configuration into appsettings
    * refactor: add typed settings
    
    * refactor: hard-coded values with settings

---
+++ Isac/Isac.Api/Settings/ClientSettings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Isac.Api.Settings
+{
+    public class ClientSettings
+    {
+        public Uri BaseUrl { get; set; }
+        public CredentialSettings Credentials { get; set; }
+    }
+}
---
+++ Isac/Isac.Api/Settings/CredentialSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace Isac.Api.Settings
+{
+    public class CredentialSettings
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+        public string AsBasicAuthorization()
+        {
+            return $"{this.UserName}:{this.Password}";
+        }
+    }
+}
